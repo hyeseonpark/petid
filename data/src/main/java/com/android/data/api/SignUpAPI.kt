8c7063c009de523206a06c02584d000dd579af5b
hyeseon <hyeseon.n.p@gmail.com>
2024-08-09 13:15:17 +0900

    login api setting

--- data/src/main/java/com/android/data/api/SignUpAPI.kt
+++ data/src/main/java/com/android/data/api/SignUpAPI.kt
@@ -1,10 +1,16 @@
 package com.android.data.api
 
-import android.telecom.Call
-import retrofit2.http.Body
+import com.android.data.model.LoginResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
+import retrofit2.Call
 
 interface SignUpAPI {
+    @FormUrlEncoded
     @POST("/auth/oauth2/login")
-    suspend fun Login(@Body login: login): Call<LoginResponse>
+    suspend fun Login(
+        @Field("sub") sub: String,
+        @Field("fcmToken") fcmToken: String
+    ): LoginResponse
 }
