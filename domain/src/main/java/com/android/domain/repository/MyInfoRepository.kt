7eb2dd57ee61d06799a0514509ee82e7fdf7ee80
Hyeseon <hyeseon.n.p@gmail.com>
2024-11-11 20:43:21 +0900

    - 마이페이지 수정 api 반영

--- domain/src/main/java/com/android/domain/repository/MyInfoRepository.kt
+++ domain/src/main/java/com/android/domain/repository/MyInfoRepository.kt
@@ -1,10 +1,12 @@
 package com.android.domain.repository
 
 import com.android.domain.entity.MemberInfoEntity
+import com.android.domain.entity.UpdateMemberInfoEntity
 import com.android.domain.util.ApiResult
 
 interface MyInfoRepository {
     suspend fun getMemberInfo(): ApiResult<MemberInfoEntity>
     suspend fun uploadProfileImage(imagePath: String): String
     suspend fun getProfileImageUrl(imagePath: String): String
+    suspend fun updateMemberInfo(): ApiResult<UpdateMemberInfoEntity>
 }
