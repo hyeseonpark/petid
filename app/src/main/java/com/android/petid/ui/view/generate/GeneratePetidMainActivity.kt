4ef943e709e6d00c969045593ca5451743fc34a1
Hyeseon <hyeseon.n.p@gmail.com>
2024-11-26 20:12:36 +0900

    - 키보드 숨기기 및 editText focus clear 기능 util 추가 - 등록대행병원 > 병원 검색 기능 추가

--- app/src/main/java/com/android/petid/ui/view/generate/GeneratePetidMainActivity.kt
+++ app/src/main/java/com/android/petid/ui/view/generate/GeneratePetidMainActivity.kt
@@ -1,14 +1,12 @@
 package com.android.petid.ui.view.generate
 
-import android.content.Context
 import android.os.Bundle
 import android.view.MotionEvent
-import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.android.petid.R
 import com.android.petid.databinding.ActivityGeneratePetidMainBinding
+import com.android.petid.util.hideKeyboardAndClearFocus
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -29,13 +27,7 @@ class GeneratePetidMainActivity : AppCompatActivity() {
      * EditText 바깥 터치 시 키보드 숨기기
      */
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
-
-        if(currentFocus is EditText) {
-            currentFocus!!.clearFocus()
-        }
-
+        hideKeyboardAndClearFocus()
         return super.dispatchTouchEvent(ev)
     }
 }
