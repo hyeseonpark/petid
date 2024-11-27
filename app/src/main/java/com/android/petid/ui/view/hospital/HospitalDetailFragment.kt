fa4f5dad5f37d06f2b4b5cea3f2a7886066214b4
hyeseon <hyeseon.n.p@gmail.com>
2024-11-27 16:14:03 +0900

    HospitalDetailFragment : image 반영

--- app/src/main/java/com/android/petid/ui/view/hospital/HospitalDetailFragment.kt
+++ app/src/main/java/com/android/petid/ui/view/hospital/HospitalDetailFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.android.petid.R
 import com.android.petid.common.BaseFragment
 import com.android.petid.databinding.FragmentHospitalDetailBinding
 import com.android.petid.viewmodel.hospital.HospitalViewModel
+import com.bumptech.glide.Glide
 
 class HospitalDetailFragment: BaseFragment<FragmentHospitalDetailBinding>(FragmentHospitalDetailBinding::inflate) {
 
@@ -34,6 +36,20 @@ class HospitalDetailFragment: BaseFragment<FragmentHospitalDetailBinding>(Fragme
 
     private fun initComponent() {
         with(viewModel.hospitalDetail) {
+            // 이미지
+            (R.drawable.img_hospital_list_empty).let {
+                val imgSource: Any? = when(imageUrl[0]) {
+                    "" -> AppCompatResources.getDrawable(requireContext(), it)
+                    else -> imageUrl[0]
+                }
+
+                Glide.with(requireContext())
+                    .load(imgSource)
+                    .placeholder(it)
+                    .error(it)
+                    .into(binding.imageViewHospitalPhoto)
+            }
+
             binding.textViewTitle.text = name
             binding.textViewVet.text = vet
             binding.textViewTime.text = hours
