9015ef0593b672a9a91e191cb6fa826647895841
hyeseon <hyeseon.n.p@gmail.com>
2024-11-30 15:41:04 +0900

    GlobalApplication: getGlobalContext() 생성  - Application Context 를 Singleton 으로 관리하기 위해 수정  - Context를 매개변수로 받아 구현하는 경우 추후 메모리 누수 문제가 발생할 수 있기에 이를 방지할 수 있게 반영함

--- app/src/main/java/com/android/petid/common/GlobalApplication.kt
+++ app/src/main/java/com/android/petid/common/GlobalApplication.kt
@@ -9,12 +9,21 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class GlobalApplication : MultiDexApplication() {
+
     override fun onCreate() {
         super.onCreate()
+
+        appInstance = this
 
         // Kakao Sdk 초기화
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
 
         Logger.addLogAdapter(AndroidLogAdapter())
     }
+
+    companion object {
+        private lateinit var appInstance: GlobalApplication
+        fun getGlobalContext() = appInstance
+    }
+
 }
