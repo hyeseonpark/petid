a4fd3313168393576c70656584c8b139d4a9675d
Hyeseon <hyeseon.n.p@gmail.com>
2024-12-05 19:37:03 +0900

    login API error.status 코드 변경 반영

--- app/src/main/java/com/android/petid/viewmodel/auth/SocialAuthViewModel.kt
+++ app/src/main/java/com/android/petid/viewmodel/auth/SocialAuthViewModel.kt
@@ -47,7 +47,7 @@ class SocialAuthViewModel @Inject constructor(
                     _loginResult.emit(LoginResult.Success(result))  // 성공 시 데이터 전송
                 }
                 is ApiResult.HttpError -> {
-                    if (result.error.status == 404 && result.error.error.contains("Member UID")) {
+                    if (result.error.status == 400 && result.error.error.contains("Member UID")) {
                         _loginResult.emit(LoginResult.NeedToSignUp)  // 회원가입 필요 시 전송
                     } else {
                         _loginResult.emit(LoginResult.Error(result.error.error))  // 오류 시 메시지 전송
