7ff33b3ab3874afef27fad92a1a6e5888de7606f
Hyeseon <hyeseon.n.p@gmail.com>
2024-12-09 16:09:11 +0900

    PetInfoRepository update

--- domain/src/main/java/com/android/domain/repository/PetInfoRepository.kt
+++ domain/src/main/java/com/android/domain/repository/PetInfoRepository.kt
@@ -1,9 +1,11 @@
 package com.android.domain.repository
 
 import com.android.domain.entity.PetDetailsEntity
+import com.android.domain.entity.PetRequestEntity
 import com.android.domain.util.ApiResult
 
 interface PetInfoRepository {
+    suspend fun registerPet(pet: PetRequestEntity): ApiResult<PetDetailsEntity>
     suspend fun getPetDetails(petId: Long): ApiResult<PetDetailsEntity>
     suspend fun getPetImageUrl(filePath: String): ApiResult<String>
 }
