e3d615defd459aa38aebbda91bdc930c3196461b
Hyeseon <hyeseon.n.p@gmail.com>
2024-12-16 18:57:52 +0900

    SignatureFragment.kt: refresh 아이콘 추가

--- app/src/main/java/com/android/petid/ui/view/generate/SignatureFragment.kt
+++ app/src/main/java/com/android/petid/ui/view/generate/SignatureFragment.kt
@@ -6,10 +6,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.android.petid.R
+import com.android.petid.common.Constants
+import com.android.petid.common.Constants.PHOTO_PATHS
+import com.android.petid.common.GlobalApplication.Companion.getPreferencesControl
 import com.android.petid.ui.view.common.BaseFragment
 import com.android.petid.databinding.FragmentSignatureBinding
+import com.android.petid.ui.state.CommonApiState
+import com.android.petid.util.Utils.bitmapToFile
+import com.android.petid.viewmodel.generate.GeneratePetidSharedViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class SignatureFragment : BaseFragment<FragmentSignatureBinding>(FragmentSignatureBinding::inflate) {
     private val viewModel: GeneratePetidSharedViewModel by activityViewModels()
@@ -32,19 +42,24 @@ class SignatureFragment : BaseFragment<FragmentSignatureBinding>(FragmentSignatu
     }
 
     fun initComponent() {
-        binding.buttonNext.setOnClickListener{
-            val bitmap = getBitmapFromView(binding.drawingViewSignature)
-
-            val memberId =
-                getPreferencesControl().getIntValue(Constants.SHARED_MEMBER_ID_VALUE)
-
-            with(viewModel) {
-                // S3 서버에 올릴 파일 세팅
-                signImage = bitmapToFile(requireContext(), bitmap, "signature.jpg")
-                petInfo.setSign("${PHOTO_PATHS[1]}${memberId}.jpg")
+        with(binding) {
+            buttonRefresh.setOnClickListener{
+                drawingViewSignature.clear()
             }
+            buttonNext.setOnClickListener{
+                val bitmap = getBitmapFromView(binding.drawingViewSignature)
 
-            viewModel.uploadImageFiles()
+                val memberId =
+                    getPreferencesControl().getIntValue(Constants.SHARED_MEMBER_ID_VALUE)
+
+                with(viewModel) {
+                    // S3 서버에 올릴 파일 세팅
+                    signImage = bitmapToFile(requireContext(), bitmap, "signature.jpg")
+                    petInfo.setSign("${PHOTO_PATHS[1]}${memberId}.jpg")
+                }
+
+                viewModel.uploadImageFiles()
+            }
         }
     }
     /**
@@ -65,40 +80,10 @@ class SignatureFragment : BaseFragment<FragmentSignatureBinding>(FragmentSignatu
         }
     }
 
-    fun getBitmapFromView(view: View): Bitmap {
+    private fun getBitmapFromView(view: View): Bitmap {
         val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
         val canvas = Canvas(bitmap)
         view.draw(canvas)
         return bitmap
     }
 }
-// 서버 전송 시
-//import android.util.Base64
-//import java.io.ByteArrayOutputStream
-//
-//fun bitmapToBase64(bitmap: Bitmap): String {
-//    val byteArrayOutputStream = ByteArrayOutputStream()
-//    bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
-//    val byteArray = byteArrayOutputStream.toByteArray()
-//    return Base64.encodeToString(byteArray, Base64.DEFAULT)
-//}
-//
-//fun sendBitmapToServer(base64Bitmap: String) {
-//    // Volley 라이브러리를 사용한 예시
-//    val url = "https://yourserver.com/upload"
-//    val request = object : StringRequest(Method.POST, url,
-//        Response.Listener { response ->
-//            // 서버 응답 처리
-//        },
-//        Response.ErrorListener { error ->
-//            // 오류 처리
-//        }) {
-//        override fun getParams(): Map<String, String> {
-//            val params = HashMap<String, String>()
-//            params["image"] = base64Bitmap
-//            return params
-//        }
-//    }
-//    val queue = Volley.newRequestQueue(context)
-//    queue.add(request)
-//}
