231616ae8f87937a9e0dda52ba9208f90e046cb5
hyeseon <hyeseon.n.p@gmail.com>
2024-12-31 22:43:29 +0900

    update NotificationActivity

--- app/src/main/java/com/petid/petid/ui/view/home/NotificationActivity.kt
+++ app/src/main/java/com/petid/petid/ui/view/home/NotificationActivity.kt
@@ -1,12 +1,23 @@
 package com.petid.petid.ui.view.home
 
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.petid.petid.R
 import com.petid.petid.databinding.ActivityNotificationBinding
+import com.petid.petid.ui.state.CommonApiState
 import com.petid.petid.ui.view.common.BaseActivity
+import com.petid.petid.util.TAG
+import com.petid.petid.viewmodel.home.NotificationViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class NotificationActivity : BaseActivity() {
     private lateinit var binding : ActivityNotificationBinding
+    private val viewModel: NotificationViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +34,30 @@ class NotificationActivity : BaseActivity() {
             showBackButton = true,
             title = resources.getString(R.string.notification_title)
         )
+        observeNoticationListState()
+        viewModel.getNotificationList()
+    }
+
+    /**
+     *
+     */
+    private fun observeNoticationListState() {
+        lifecycleScope.launch {
+            viewModel.notificationListState.collectLatest { result ->
+               if (result != CommonApiState.Loading)
+                    hideLoading()
+
+                when(result) {
+                    is CommonApiState.Success -> {
+                        Log.d(TAG, result.data.size.toString())
+                    }
+                    is CommonApiState.Error -> {
+                        Log.e(TAG, result.message.toString())
+                    }
+                    CommonApiState.Loading -> showLoading()
+                    CommonApiState.Init -> {}
+                }
+            }
+        }
     }
 }
