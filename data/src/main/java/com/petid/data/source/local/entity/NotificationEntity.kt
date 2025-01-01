5b736518f7505502b7386c960df2a421560aa531
hyeseon <hyeseon.n.p@gmail.com>
2025-01-01 18:36:45 +0900

    update AppFirebaseMessageService

--- data/src/main/java/com/petid/data/source/local/entity/NotificationEntity.kt
+++ data/src/main/java/com/petid/data/source/local/entity/NotificationEntity.kt
@@ -6,18 +6,18 @@ import androidx.room.PrimaryKey
 /**
  *
  * [docs](https://petid.atlassian.net/wiki/x/MIBLAQ)
- * @param type 알람 타입
- * @param desc 내용
+ * @param title 제목
+ * @param body 내용
  * @param category 분류 (booking : 관리자 예약 확인, order : 방문 전후 알림, reminder: 리마인더 (status: null))
  * @param status 상태 (PENDING, CONFIRMED, CANCELED, COMPLETED)
  * @param isChecked 읽음 여부
  */
 @Entity(tableName = "notifications")
 data class NotificationEntity(
-    val type: String,
-    val desc: String,
+    val title: String,
+    val body: String,
     val category: String,
-    val status: String,
+    val status: String?,
     val isChecked: Boolean = false,
 ) {
     @PrimaryKey(autoGenerate = true)
