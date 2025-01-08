f78d16b5b37b27b67947c3fcd4be818c55049ec4
Hyeseon <hyeseon.n.p@gmail.com>
2025-01-08 17:54:34 +0900

    create Result<T>.mapDBResult<T>

--- data/src/main/java/com/petid/data/util/Extensions.kt
+++ data/src/main/java/com/petid/data/util/Extensions.kt
@@ -4,15 +4,16 @@ import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.gson.Gson
 import com.petid.data.dto.response.ErrorResponse
 import com.petid.data.dto.response.toDomain
+import com.petid.data.repository.local.DBResult
 import com.petid.domain.util.ApiResult
 import retrofit2.HttpException
 
 /**
- * Result<T>를 ApiResult<T> 로 변환한다.
+ * Result<T>를 ApiResult<T> 로 반환한다.
  * 이 때, onFailure 결과를 Exception 타입에 따라 ApiResult를 다르게 반환하는 작업을 공통화 하였다.
  */
-inline fun <T> Result<T>.mapApiResult(transform: (T) -> ApiResult<T>): ApiResult<T> {
-    return fold(
+inline fun <T> Result<T>.mapApiResult(transform: (T) -> ApiResult<T>): ApiResult<T> =
+    fold(
         onSuccess = transform,
         onFailure = { error ->
             when (error) {
@@ -28,7 +29,17 @@ inline fun <T> Result<T>.mapApiResult(transform: (T) -> ApiResult<T>): ApiResult
             }
         }
     )
-}
+
+/**
+ * Result<T>를 DBResult<T>로 반환한다.
+ */
+inline fun <T> Result<T>.mapDBResult(transform : (T) -> DBResult<T>): DBResult<T> =
+    fold(
+        onSuccess = transform,
+        onFailure = { error ->
+            DBResult.Error(error)
+        }
+    )
 
 /**
  * Crashlytics 기록
