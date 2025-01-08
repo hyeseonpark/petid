85f650f6848f69b91a18800b92cdddbfcf44f9da
hyeseon <hyeseon.n.p@gmail.com>
2025-01-08 17:54:34 +0900

    create Result<T>.mapDBResult<T>

--- data/src/main/java/com/petid/data/repository/local/NotificationRepository.kt
+++ data/src/main/java/com/petid/data/repository/local/NotificationRepository.kt
@@ -2,6 +2,7 @@ package com.petid.data.repository.local
 
 import com.petid.data.source.local.dao.NotificationDao
 import com.petid.data.source.local.entity.NotificationEntity
+import com.petid.data.util.mapDBResult
 import jakarta.inject.Inject
 import javax.inject.Singleton
 
@@ -10,44 +11,29 @@ import javax.inject.Singleton
 class NotificationRepository @Inject constructor(
     private val notificationDao: NotificationDao,
 ) {
-    suspend fun getNotifications(): DBResult<List<NotificationEntity>> {
-        return try {
-            val result = notificationDao.getAllNotifications()
-            DBResult.Success(result)
-        } catch (e: Exception) {
-            DBResult.Error(e)
-        }
-    }
+    suspend fun getNotifications(): DBResult<List<NotificationEntity>> =
+        runCatching {
+            notificationDao.getAllNotifications()
+        }.mapDBResult { DBResult.Success(it) }
 
-    suspend fun saveNotification(notificationEntity: NotificationEntity): DBResult<Unit> {
-        return try {
-            val result = notificationDao.insertNotification(notificationEntity)
-            DBResult.Success(result)
-        } catch (e: Exception) {
-            DBResult.Error(e)
-        }
-    }
+    suspend fun saveNotification(notificationEntity: NotificationEntity): DBResult<Unit> =
+        runCatching {
+            notificationDao.insertNotification(notificationEntity)
+        }.mapDBResult { DBResult.Success(it) }
 
-    suspend fun markAsChecked(notificationId: Long): DBResult<List<NotificationEntity>> {
-        return try {
-            val result = notificationDao.updateAndFetchNotifications(notificationId)
-            DBResult.Success(result)
-        } catch (e: Exception) {
-            DBResult.Error(e)
-        }
-    }
+    suspend fun markAsChecked(notificationId: Long): DBResult<List<NotificationEntity>> =
+        runCatching {
+            notificationDao.updateAndFetchNotifications(notificationId)
+        }.mapDBResult { DBResult.Success(it) }
 
-    suspend fun hasUncheckedNotification(): DBResult<Boolean> {
-        return try {
-            val result = notificationDao.hasUncheckedNotification()
-            DBResult.Success(result)
-        } catch (e: Exception) {
-            DBResult.Error(e)
-        }
-    }
+    suspend fun hasUncheckedNotification(): DBResult<Boolean> =
+        runCatching {
+            notificationDao.hasUncheckedNotification()
+        }.mapDBResult { DBResult.Success(it) }
+
 }
 
 sealed class DBResult<out T> {
     data class Success<T>(val data: T) : DBResult<T>()
-    data class Error(val exception: Exception) : DBResult<Nothing>()
+    data class Error(val exception: Throwable) : DBResult<Nothing>()
 }
