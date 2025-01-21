d9df9cf41f9b16386627036c74c8c3236cd1f399
Hyeseon <hyeseon.n.p@gmail.com>
2025-01-21 13:51:04 +0900

    update getBannerImage

--- domain/src/main/java/com/petid/domain/repository/HomeMainRepository.kt
+++ domain/src/main/java/com/petid/domain/repository/HomeMainRepository.kt
@@ -5,6 +5,5 @@ import com.petid.domain.util.ApiResult
 
 interface HomeMainRepository {
     suspend fun getBannerList(type: String): ApiResult<List<BannerEntity>>
-    suspend fun getBannerImage(imagePath: String): String
-//    suspend fun getBannerImage(imagePath: String): ApiResult<String>
+    suspend fun getBannerImage(imagePath: String): ApiResult<String>
 }
