82644da217af3bee65cbe805962952a1ad2b9bbb
Hyeseon <hyeseon.n.p@gmail.com>
2025-02-10 16:03:49 +0900

    update code

--- app/src/main/java/com/petid/petid/GlobalApplication.kt
+++ app/src/main/java/com/petid/petid/GlobalApplication.kt
@@ -35,7 +35,6 @@ class GlobalApplication : MultiDexApplication() {
                 }
             }
         }
-
     }
 
     companion object {
--- domain/src/main/java/com/petid/domain/usecase/GetCommonInfoListUseCase.kt
+++ domain/src/main/java/com/petid/domain/usecase/GetCommonInfoListUseCase.kt
@@ -1,8 +1,6 @@
 package com.petid.domain.usecase
 
-import com.petid.domain.entity.CommonInfo
 import com.petid.domain.repository.BlogMainRepository
-import kotlinx.coroutines.flow.Flow
 
 class GetCommonInfoListUseCase(
     private val blogMainRepository: BlogMainRepository,
